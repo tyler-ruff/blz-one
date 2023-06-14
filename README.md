929f62ff4f3114db1ce17a658a2adaa58a4580b1
Tyler Ruff <hello@blazed.space>
2023-06-14 03:44:38 -0400

    BLZ

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # blz.one
 A virtual authorization site.
 
+## Features
+- User authentication (Auth API/SDK)
+- Posts (+ Share API/SDK)
+- Comments (+ Comment API/SDK)
+- Hearts (+ Hearts API/SDK)
+- 
+
 ## Utils
-* Firebase
+* [Vue 3](https://vuejs.org/), [vue-router](https://router.vuejs.org/) & [Vite](https://vitejs.dev/)
+* [Firebase](https://firebase.google.com/)
 * [Topbar](https://github.com/buunguyen/topbar)
--- src/components/one/Hero.vue
+++ src/components/one/Hero.vue
@@ -3,10 +3,10 @@
 </script>
 <template>
     <article class="hero">
-        <h1 class="text-3xl text-blue-700 sm:text-5xl">
+        <h1 class="block text-3xl text-blue-700 sm:text-5xl">
             {{ title }}
         </h1>
-        <p class="mt-4 sm:text-xl/relaxed px-10 md:px-20">
+        <p class="block mt-4 sm:text-xl/relaxed px-10 md:px-20">
             {{ description }}
         </p>
         <div class="action-buttons mt-8 flex flex-wrap justify-center gap-4 px-10 md:px-0">
@@ -18,6 +18,7 @@
     .hero{
         padding-top:60px;
         padding-bottom:60px;
+        display:block;
     }
     .action-buttons a{
         display:block;

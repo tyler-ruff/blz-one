152d00cad75409033987afb5fecdbccd67062112
Tyler Ruff <hello@blazed.space>
2023-07-10 23:01:31 -0400

    test

---
+++ components/Markdown.vue
@@ -0,0 +1,18 @@
+<script setup>
+    const slug = ref('');
+</script>
+<template>
+    <ContentDoc :path="slug">
+        <template v-slot="{ doc }">
+            <h1 class="docs-title">
+                {{ doc.title }}
+            </h1>
+            <ContentRenderer :value="doc"/>
+        </template>
+        <template #not-found> 
+            <h1>
+                404 not found
+            </h1> 
+        </template>
+    </ContentDoc>
+</template>
---
+++ pages/docs/hello.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import '~/assets/css/docs.css'
+    definePageMeta({
+        layout: "docs",
+    });
+</script>
+<template>
+    <div class="docs">
+        <Markdown slug="hello" />
+    </div>
+</template>

11f50459da06b8464e13561e046e7079d78c484b
Tyler Ruff <hello@blazed.space>
2023-07-10 22:37:20 -0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ const firebaseConfig = {
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: false,
+  ssr: true,
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss',
@@ -34,10 +34,10 @@ export default defineNuxtConfig({
       }
   },
   nitro: {
-    preset: 'firebase',
+    preset: 'node-server',
     prerender: {
       crawlLinks: true
-    }
+    },
   },
   colorMode: {
     classSuffix: ''
